e3b2bcdd4b9802296f77b5340e54fcefb1954ad2
Joao Pedro <jp3351207@gmail.com>
2024-08-23 15:08:52 -0300

    Departamento entidade class

---
+++ src/aplicaçao/Programa.java
@@ -0,0 +1,13 @@
+package aplicaçao;
+
+import modelo.entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Departamento obj = new Departamento(1, "livros");
+		System.out.println(obj);
+	}
+
+}
